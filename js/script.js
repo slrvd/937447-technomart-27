b61666d26f5e240dfcd2c2efc5cead68f24e2113
slrvd <58954337+slrvd@users.noreply.github.com>
2020-03-14 17:12:48 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 var feedbackPopup = document.querySelector(".feedback");
 var feedbackButton = document.querySelector(".feedback-button");
 var feedbackClose = document.querySelector(".feedback-close");
-var feedbackCancel = document.querySelector(".feedback-cancel");
 
-if (feedbackButton != null && feedbackClose != null && feedbackCancel != null) {
-    feedbackButton.addEventListener("click", function (event) {
-        event.preventDefault(event);
-        feedbackPopup.classList.add("show-block");
-    });
+if (feedbackButton != null && feedbackClose != null) {
+	feedbackButton.addEventListener("click", function (event) {
+		event.preventDefault(event);
+		feedbackPopup.classList.add("show-block");
+	});
 
-    feedbackClose.addEventListener("click", function (event) {
-        event.preventDefault(event);
-        feedbackPopup.classList.remove("show-block");
-    });
+	feedbackClose.addEventListener("click", function (event) {
+		event.preventDefault(event);
+		feedbackPopup.classList.remove("show-block");
+	});
 
-    feedbackCancel.addEventListener("click", function (event) {
-        event.preventDefault(event);
-        feedbackPopup.classList.remove("show-block");
-    });
 
-    window.addEventListener("keydown", function (event) {
-        if (event.keyCode == 27) {
-            if (feedbackPopup.classList.contains("show-block")) {
-                feedbackPopup.classList.remove("show-block");
-            }
-        }
-    });
+	window.addEventListener("keydown", function (event) {
+		if (event.keyCode == 27) {
+			if (feedbackPopup.classList.contains("show-block")) {
+				feedbackPopup.classList.remove("show-block");
+			}
+		}
+	});
 }
 
-var buyButtons = document.querySelectorAll(".catalog-item .buy"), i;
+var buyButtons = document.querySelectorAll(".catalog-item .buy"),
+	i;
 var cartBlock = document.querySelector(".cart-popup");
 var cartClose = document.querySelector(".cart-popup-close");
 var cartCancel = document.querySelector(".cart-cancel-btn");
@@ -51,11 +47,11 @@ cartCancel.addEventListener("click", function (event) {
 });
 
 window.addEventListener("keydown", function (event) {
-    if (event.keyCode == 27) {
-        if (cartBlock.classList.contains("show-block")) {
-            cartBlock.classList.remove("show-block");
-        }
-    }
+	if (event.keyCode == 27) {
+		if (cartBlock.classList.contains("show-block")) {
+			cartBlock.classList.remove("show-block");
+		}
+	}
 });
 
 var mapBlock = document.querySelector(".map");
@@ -73,30 +69,30 @@ mapClose.addEventListener("click", function (event) {
 });
 
 window.addEventListener("keydown", function (event) {
-    if (event.keyCode == 27) {
-        if (mapBlock.classList.contains("show-block")) {
-            mapBlock.classList.remove("show-block");
-        }
-    }
+	if (event.keyCode == 27) {
+		if (mapBlock.classList.contains("show-block")) {
+			mapBlock.classList.remove("show-block");
+		}
+	}
 });
 
 function initialize() {
-    var x = 59.938969;
-    var y = 30.323040;
-    var mapOptions = {
-        zoom: 17,
-        center: new google.maps.LatLng(x, y),
-        scrollwheel: false,
-        disableDefaultUI: true
-    }
-    var map = new  google.maps.Map(
-        document.querySelector(".map-script"),
-        mapOptions
-    );
-    var myLatLng = new google.maps.LatLng(x, y);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map
-    });
+	var x = 59.938969;
+	var y = 30.323040;
+	var mapOptions = {
+		zoom: 17,
+		center: new google.maps.LatLng(x, y),
+		scrollwheel: false,
+		disableDefaultUI: true
+	}
+	var map = new google.maps.Map(
+		document.querySelector(".map-script"),
+		mapOptions
+	);
+	var myLatLng = new google.maps.LatLng(x, y);
+	var beachMarker = new google.maps.Marker({
+		position: myLatLng,
+		map: map
+	});
 }
 google.maps.event.addDomListener(mapLink, "click", initialize);
